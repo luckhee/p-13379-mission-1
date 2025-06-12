01f4c9f639e196c97edb9889c27529db33679825
luckhee <dcs1206@naver.com>
2025-06-12 22:37:09 +0900

    14단계

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -1,26 +1,84 @@
 package src.main.java.com.back;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calc {
     static int result = 0;
+
     static int run(String s) {
+        List<String> numList = new ArrayList<>();
+        List<String> signList = new ArrayList<>();
+        int cnt =0;
+
+        // 음수 계산할것~
+        //s=s.replaceAll(" ","");
+
+        signList = findSign(s, signList);
+        numList = findNum(s);
+
+        // 첫번째 숫자는 무조건 더해버려
+        result += Integer.parseInt(numList.get(cnt));
+        numList.remove(cnt);
+
+        calc(cnt, numList, signList);
 
 
-        s=s.replaceAll(" ",""); // 2+2
-
-
-        if(s.isBlank()) {
-            return result;
-        }
-        else {
-            if(s.contains("+")) {
-                String[] splitString = s.split("\\+");
-                result += Integer.parseInt(splitString[0]);
-                run(splitString[1]);
-            } else {
-                result +=Integer.parseInt(s);
-            }
-
-        }
         return result;
+    }
+
+    private static int calc(int cnt, List<String> numList, List<String> signList) {
+
+        if(numList.isEmpty()) return result;
+        else {
+            if(signList.get(cnt).equals("+")) {
+                result += Integer.parseInt(numList.get(cnt));
+                numList.remove(cnt);
+                signList.remove(cnt);
+
+            } else if(signList.get(cnt).equals("-")) {
+                result -= Integer.parseInt(numList.get(cnt));
+                numList.remove(cnt);
+                signList.remove(cnt);
+
+            } else if(signList.get(cnt).equals("*")) {
+                result *= Integer.parseInt(numList.get(cnt));
+                numList.remove(cnt);
+                signList.remove(cnt);
+            }
+        }
+        calc(cnt,numList,signList);
+
+        return result;
+    }
+
+    private static List<String> findNum(String s) {
+        String[] trim = s.split(" "); // [10, *, -10]
+        List<String> sortNum = new ArrayList<>();
+
+        for(String x : trim) {
+            if(!x.equals("+") && !x.equals("-") && !x.equals("*")) {
+                sortNum.add(x);
+            }
+        }
+        return sortNum;
+    }
+
+
+
+
+    private static List<String> findSign(String s, List<String> signList) {
+        //1+1
+        String[] trim = s.split(" "); // [10,*,-10]
+
+        for(String x : trim) { //1+1
+            try {
+                Integer.parseInt(x);
+            } catch (NumberFormatException e) {
+                signList.add(x);
+            }
+        }
+
+        return signList;
     }
 }
--- src/test/java/com/back/CalcTest.java
+++ src/test/java/com/back/CalcTest.java
@@ -11,7 +11,9 @@ public class CalcTest {
     @Test
     @DisplayName("1 + 1 == 2")
     void t1() {
-        assertThat(ReCalc.run("1 + 1")).isEqualTo(2);
+        assertThat(Calc.
+                run("1 + 1"))
+                .isEqualTo(2);
     }
 
     @Test
@@ -29,13 +31,16 @@ public class CalcTest {
     @Test
     @DisplayName("1000 + 280 == 1280")
     void t4() {
-        assertThat(Calc.run("1000 + 280")).isEqualTo(1280);
+        assertThat(Calc.
+                run("1000 + 280")).
+                isEqualTo(1280);
     }
 
     @Test
     @DisplayName("2 - 1 == 1")
     void t5() {
-        assertThat(Calc.run("2 - 1")).isEqualTo(1);
+        assertThat(Calc.
+                run("2 - 1")).isEqualTo(1);
     }
 
     @Test
@@ -53,13 +58,17 @@ public class CalcTest {
     @Test
     @DisplayName("10 + 20 + 30 == 60")
     void t8() {
-        assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
+        assertThat(Calc.
+                run("10 + 20 + 30")).
+                isEqualTo(60);
     }
 
     @Test
     @DisplayName("10 - 20 + 30 == 20")
     void t9() {
-        assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
+        assertThat(Calc.
+                run("10 - 20 + 30")).
+                isEqualTo(20);
     }
 
     @Test
@@ -77,13 +86,16 @@ public class CalcTest {
     @Test
     @DisplayName("10 * 10 == 100")
     void t12() {
-        assertThat(Calc.run("10 * 10")).isEqualTo(100);
+        assertThat(Calc.
+                run("10 * 10"))
+                .isEqualTo(100);
     }
 
     @Test
     @DisplayName("10 * -10 == -100")
     void t13() {
-        assertThat(Calc.run("10 * -10")).isEqualTo(-100);
+        assertThat(Calc.
+                run("10 * -10")).isEqualTo(-100);
     }
 
     @Test
@@ -91,6 +103,8 @@ public class CalcTest {
     void t14() {
         assertThat(Calc.run("10 * 10 * 10")).isEqualTo(1000);
     }
+
+
 
 
 }
