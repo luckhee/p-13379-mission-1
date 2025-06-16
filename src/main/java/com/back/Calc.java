973e41403f34adc7585ea26395c08fd032d3d047
luckhee <dcs1206@naver.com>
2025-06-16 23:11:10 +0900

    stateless

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -1,55 +1,51 @@
-package src.main.java.com.back;
+package com.back;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Calc {
-    static int result = 0;
+
 
     static int run(String s) {
         List<String> numList = new ArrayList<>();
         List<String> signList = new ArrayList<>();
         int cnt =0;
 
-        // 음수 계산할것~
-        //s=s.replaceAll(" ","");
+
 
         signList = findSign(s, signList);
         numList = findNum(s);
 
-        // 첫번째 숫자는 무조건 더해버려
-        result += Integer.parseInt(numList.get(cnt));
+
+        int result = Integer.parseInt(numList.get(cnt));
         numList.remove(cnt);
 
-        calc(cnt, numList, signList);
+
+
+        result = calc(cnt, numList, signList, result);
 
 
         return result;
     }
 
-    private static int calc(int cnt, List<String> numList, List<String> signList) {
+    private static int calc(int cnt, List<String> numList, List<String> signList, int result) {
 
         if(numList.isEmpty()) return result;
-        else {
-            if(signList.get(cnt).equals("+")) {
-                result += Integer.parseInt(numList.get(cnt));
-                numList.remove(cnt);
-                signList.remove(cnt);
 
-            } else if(signList.get(cnt).equals("-")) {
-                result -= Integer.parseInt(numList.get(cnt));
-                numList.remove(cnt);
-                signList.remove(cnt);
+        String op = signList.get(cnt);
+        int num = Integer.parseInt(numList.get(cnt));
 
-            } else if(signList.get(cnt).equals("*")) {
-                result *= Integer.parseInt(numList.get(cnt));
-                numList.remove(cnt);
-                signList.remove(cnt);
-            }
+        switch(op) {
+            case "+" -> result += num;
+            case "-" -> result -= num;
+            case "*" -> result *= num;
         }
-        calc(cnt,numList,signList);
 
-        return result;
+        numList.remove(cnt);
+        signList.remove(cnt);
+        return calc(cnt,numList,signList, result);
+
+
     }
 
     private static List<String> findNum(String s) {
